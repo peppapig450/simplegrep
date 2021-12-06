3cb17f44488c33e3f04a5acc515d843f5cb02335
peppapig450 <nbgenius1@gmail.com>
2021-12-05 18:00:30 -0800

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+extern crate simplegrep;
+
+use std::env;
+use std::process;
+use simplegrep::Config;
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    }); 
+
+    println!("I'm searching for '{}'", config.query);
+    println!("in file{}", config.filename);
+
+    if let Err(e) = simplegrep::run(config) {
+        eprintln!("Application Error: {}", e);
+        process::exit(1);
+    }
+}
